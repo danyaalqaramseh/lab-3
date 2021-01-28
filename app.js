0bbc3eba1f28425c46edede168763431f1e3349a
danyaalqaramseh <d.alqaramseh98@gmial.com>
2021-01-28 04:41:40 -0800

    lab3+4+5

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var AnimeName = prompt("Type the name of the Anime please");
 
 var welc;
 
-if (personalType == "YES" || personalType == "yes")
-{
+if (personalType == "YES" || personalType == "yes") {
     welc = "welcome to your favorite anime store";
 
 }
@@ -18,3 +17,31 @@ else if (personalType == "no" || personalType == "NO") {
 document.write(welc);
 
 confirm("Welcome to your store");
+
+
+
+var clientType = prompt('Are You interested in anime or video games ?');
+
+while (clientType == 'anime' || clientType == 'video games') {
+    clientType = prompt('Welcome to your store . you can find what you want');
+}
+while (clientType !== 'anime' && clientType !== 'video games') {
+    clientType = prompt('what are you interested in ?');
+
+}
+
+var itemType = '';
+
+if (clientType === 'anime') {
+    itemType = prompt('are you interested in figures or something else');
+
+    while (itemType == 'figures') {
+
+        document.write("some figures");
+        itemType = '<img src = "images/Anime.jpg"/>';
+        document.write(itemType);
+    }
+
+
+}
+
